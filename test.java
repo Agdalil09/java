d624c0b5ea3c3bf835c05713e35d3430bd392991
Agdalil09 <liladagli09@gmail.com>
2023-02-18 22:27:14 +0100

    premiere sauvegarde

---
+++ test.java
@@ -0,0 +1,35 @@
+package tp0;
+
+public class Date {
+    int jour,mois,annee;
+
+public int get_jour()
+{return (jour);}
+public int get_mois()
+{return (mois);}
+public int get_annee()
+{return (annee);}
+
+public void set_jour(int j)
+{jour=j;}
+public void set_mois(int m)
+{mois=m;}
+public void set_annee(int a)
+{annee=a;}
+public void set_date(int a,int b,int c)
+{jour=a;
+mois=b;
+annee=c;}
+
+public date(){};
+
+public date(int jour,int m,int annee)
+{this.jour=jour;
+mois=m;
+this.annee=annee;}
+
+public void affiche(){
+	System.out.println(jour+"/"+mois+"/"+annee);}
+
+}
+
